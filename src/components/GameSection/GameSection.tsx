19dc12f20472df5e8b4456a57266267f69fc82eb
Anton Shvetsov <anton.shvetsov@mail.ru>
2022-08-16 15:18:50 +0300

    added crossbrowsering for main page

--- src/components/GameSection/GameSection.tsx
+++ src/components/GameSection/GameSection.tsx
@@ -11,10 +11,14 @@ import 'swiper/scss';
 import 'swiper/scss/navigation';
 import 'swiper/scss/pagination';
 import 'swiper/scss/effect-fade';
+import useMediaQuery from '../../hooks/useMediaQuery';
 
 export function GameSection () {
   const urls = useSelector((state: IState) => state.urls);
   const dispatch = useDispatch();
+  const isDesktop = useMediaQuery('(min-width: 960px)');
+  const isMobile = useMediaQuery('(min-width: 560px)');
+  const isLargeDesktop = useMediaQuery('(min-width: 1280px)');
 
   useEffect(() => {
     (async () => {
@@ -27,14 +31,15 @@ export function GameSection () {
     <Swiper
       modules={[Navigation, Pagination, Scrollbar, A11y]}
       spaceBetween={50}
-      slidesPerView={3.01}
+      slidesPerView={isLargeDesktop?4.1:isDesktop?3.01:isMobile? 2: 1}
       watchSlidesProgress={true}
       loop={true}
       scrollbar={{ draggable: true }}
     >
-      {urls.map((url: IUrl) => <SwiperSlide key={url.id}>
-        <Game imageUrl={url.url}/>
-      </SwiperSlide>)}
+      {urls.map((url: IUrl) => 
+        <SwiperSlide key={url.id}>
+          <Game imageUrl={url.url}/>
+        </SwiperSlide>)}
     </Swiper>
   );
 }
