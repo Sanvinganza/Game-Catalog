52fb085e12489a2876bcb4a89bd2b36da913a541
Sanvinganza <anton.shvetsov@mail.ru>
2022-10-04 01:36:34 +0300

    fixed bag in dropdown buttons

--- src/components/FilterWrapper/DropdownButtons/index.tsx
+++
@@ -1,7 +0,0 @@
-import { platforms, ratingIGDB, themes, years } from "../../../helper/constants";
-import { DropdownButtonFilter } from "./DropdownButtonFilter";
-
-export const DropdownButtonRaiting = DropdownButtonFilter(ratingIGDB, "Raiting");
-export const DropdownButtonYears = DropdownButtonFilter(years, "Years");
-export const DropdownButtonPlatforms = DropdownButtonFilter(platforms, "Platforms");
-export const DropdownButtonThemes = DropdownButtonFilter(themes, "Themes");
